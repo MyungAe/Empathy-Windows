59b85127c95cfdf77cf4a98f108a84da5c084fb5
JaeranKIM <devjaeran@gmail.com>
2023-02-12 00:49:43 +0900

    댓글 기능 합본, 댓글 삭제까지 구현 완료

--- app.py
+++ app.py
@@ -1,72 +1,3 @@
-from flask import Flask, render_template, request, jsonify
-
-app = Flask(__name__)
-
-from pymongo import MongoClient
-import certifi
-
-ca = certifi.where()
-client = MongoClient(
-    'mongodb+srv://project4-Empathy-Windows:Empathy@cluster0.z57zxvu.mongodb.net/?retryWrites=true&w=majority',
-    tlsCAFile=ca)
-db = client.empathy
-
-
-@app.route('/music')
-def music():
-    return render_template('index.html')
-
-@app.route('/comment')
-def comment():
-    return render_template('comment.html')
-
-
-# 댓글 저장
-@app.route("/music/comment", methods=["POST"])
-def comment_post():
-    comment_receive = request.form['comment_give']
-    date_receive = request.form['date_give']
-
-    comment_list = list(db.comments.find({}, {'_id': False}))
-    count = len(comment_list) + 1
-
-    # 회원정보들 = list(db.'회원정보들'.find({}, {'_id': False}))
-    # for 회원정보 in 회원정보들:
-    #     nickname = 회원정보['nickname']
-    #
-    # 음악목록들 = list(db.'음악목록들'.find({}, {'_id': False}))
-    # for 음악목록 in 음악목록들:
-    #     music_name  = 회원정보['music_name ']
-
-    doc = {
-        'nickname': 'ksg',
-        'num': count,
-        'comment': comment_receive,
-        'date': date_receive,
-        'music_name': 'human',
-    }
-
-    db.comments.insert_one(doc)
-
-    return jsonify({'msg': '등록 완료!!!'})
-
-
-# 댓글 조회
-@app.route("/music/comment", methods=["GET"])
-def comment_get():
-    comment_list = list(db.comments.find({}, {'_id': False}))
-
-    return jsonify({'comments': comment_list})
-
-
-# 댓글 삭제
-@app.route("/music/comment", methods=["DELETE"])
-def comment_de():
-    num_receive = request.form['num_give']
-    db.comments.delete_one({'num': num_receive})
-    return jsonify({'msg': '삭제 완료!!'})
-
-== == == =
 # Flask 서버 사용을 위한 import
 from flask import Flask, render_template, request, jsonify
 
@@ -133,13 +64,13 @@ def account_signin2():
     return jsonify({'msg': '로그인성공'})
 
 
-# 홈페이지 불러오기
+# 음악 창구 홈페이지 불러오기
 @app.route('/musics')
 def music():
     return render_template('musicpage.html')
 
 
-# 보여주기
+# 음악 보여주기
 musicLists = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
 for musicList in musicLists:
     a = musicList.select_one('td.info > a.title.ellipsis').text.strip().lstrip()
@@ -154,7 +85,6 @@ for musicList in musicLists:
         }
         user_db.music.insert_one(doc)
 
-
 @app.route("/music", methods=["GET"])
 def music_get():
     musicList = list(user_db.music.find({}, {'_id': False}))
@@ -162,15 +92,78 @@ def music_get():
     return jsonify({'music': musicList[:20]})
 
 
-# 수정하기
-@app.route("/music/comment/", methods=["FATCH"])
+# 댓글 저장
+@app.route("/music/comment", methods=["POST"])
+def comment_post():
+    comment_receive = request.form['comment_give']
+    date_receive = request.form['date_give']
+
+    comment_list = list(user_db.comments.find({}, {'_id': False}))
+    count = len(comment_list) + 1
+
+    # 회원정보들 = list(db.'회원정보들'.find({}, {'_id': False}))
+    # for 회원정보 in 회원정보들:
+    #     nickname = 회원정보['nickname']
+    #
+    # 음악목록들 = list(db.'음악목록들'.find({}, {'_id': False}))
+    # for 음악목록 in 음악목록들:
+    #     music_name  = 회원정보['music_name ']
+
+    doc = {
+        'nickname': 'ksg',
+        'num': count,
+        'comment': comment_receive,
+        'date': date_receive,
+        'music_name': 'human',
+    }
+
+    user_db.comments.insert_one(doc)
+
+    return jsonify({'msg': '등록 완료!!!'})
+
+
+# 댓글 조회
+@app.route("/music/comment", methods=["GET"])
+def comment_get():
+    comment_list = list(user_db.comments.find({}, {'_id': False}))
+
+    return jsonify({'comments': comment_list})
+
+
+# 댓글 수정
+@app.route("/music/comment", methods=["PATCH"])
 def comment_update():
     comment_receive = request.form["comment_give"]
     print(comment_receive)
-    db.comments.update_one({'name': 'Mercedes Tyler'}, {'$set': {'text': comment_receive}})
+    user_db.comments.update_one({'name': 'Mercedes Tyler'}, {'$set': {'text': comment_receive}})
     return jsonify({'msg': '수정되었습니다!'})
 
->> >> >> > main
+
+# 댓글 삭제
+@app.route("/music/comment", methods=["DELETE"])
+def comment_del():
+    num_receive = request.form['num_give']
+    print(type(num_receive))
+    user_db.comments.delete_one({'num': int(num_receive)})
+    return jsonify({'msg': '삭제 완료!!'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
+
+
+
+###
+# @app.route('/music')
+# def music():
+#     return render_template('index.html')
+
+# @app.route('/comment')
+# def comment():
+#     return render_template('comment.html')
+
+
+
+
+
--- static/scripts/musicpage.js
+++ static/scripts/musicpage.js
@@ -1,6 +1,7 @@
 //get music data
 $(document).ready(function () {
     show_music();
+    show_comment();
 });
 
 function show_music() {
@@ -29,14 +30,71 @@ function show_music() {
 }
 
 //댓글 저장
+function save_comment() {
+    let comment = $('#comment').val()
+
+    //날짜 코드
+    let today = new Date();
+    let year = String(today.getFullYear());
+    let mon = (('0' + (today.getMonth() + 1)).slice(-2));
+    let date = ('0' + (today.getDate())).slice(-2);
+    let fullDate = `${year}-${mon}-${date}`
+
+    $.ajax({
+        type: "POST",
+        url: "/music/comment",
+        data: {comment_give: comment, date_give: fullDate},
+        success: function (response) {
+            alert(response["msg"])
+            window.location.reload()
+        }
+    });
+}
+
+//댓글 보기
+function show_comment() {
+    $.ajax({
+        type: "GET",
+        url: "/music/comment",
+        data: {},
+        success: function (response) {
+            console.log(response)
+            let rows = response['comments']
+            for (let i = 0; i < rows.length; i++) {
+                let nickname = rows[i]['nickname']
+                let num = rows[i]['num']
+                let comment = rows[i]['comment']
+                let date = rows[i]['date']
+
+                let temp_html = `<div class="commentcard" >
+                                    <div class="card-header">
+                                        <p>${date}</p>
+                                         <div class="card-header-btns">
+                                            <button onclick="commentPatch()">수정하기</button>
+                                            <button onclick="del_comment(${num})">삭제하기</button>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <blockquote class="blockquote mb-0">
+                                            <p>${num}- ${comment}</p>
+                                            <footer class="blockquote-footer">${nickname}</cite>
+                                            </footer>
+                                        </blockquote>
+                                    </div>
+                                </div>`
+                $('#comment-list').append(temp_html)
+            }
+        }
+    });
+}
 
 //댓글 수정
 function commentPatch() {
     let update = prompt('수정사항을 입력해주세요.')
     alert(update)
     $.ajax({
-        type: "FATCH",
-        url: "/music/comment/",
+        type: "PATCH",
+        url: "/music/comment",
         data: {comment_give: update},
         success: function (response) {
             console.log(response["msg"])
@@ -45,3 +103,14 @@ function commentPatch() {
 }
 
 //댓글 삭제
+function del_comment(num) {
+    $.ajax({
+        type: 'DELETE',
+        url: '/music/comment',
+        data: {num_give: num},
+        success: function (response) {
+            alert(response['msg'])
+            window.location.reload()
+        }
+    });
+}
