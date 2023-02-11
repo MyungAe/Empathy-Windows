0a8097afa68ffbaf88a2e7c09166154a5bd3ee8b
19Edwin92 <b_a_s_e@naver.com>
2023-02-12 03:52:56 +0900

    ver08

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -5,29 +5,6 @@ function login_singup() {
       window.open('/account/signup2', 'open', 'width=500, height=300, top ='+top+', left='+left);
 }
 
-function signup() {
-  let id = document.querySelector('#signup_box_id').value
-  let pw1 = document.querySelector('#signup_box_pw1').value
-  let pw2 = document.querySelector('#signup_box_pw2').value
-  let nick = document.querySelector('#signup_box_nickName').value
-  
-  if(pw1 === pw2) {
-    $.ajax({
-      type:"POST",
-      url:"/account/signup2check",
-      data:{'id_give':id,'pw_give':pw1,'nick_give':nick},
-      success:function(response) {
-        alert(response['msg'])
-        window.close()
-      }
-    })
-  } else {
-    alert("비밀번호가 일치하지 않습니다.")
-  }
-
-
-}
-
 function login() {
     let id = document.querySelector('#login_box_id').value
     let pw = document.querySelector('#login_box_pw').value
