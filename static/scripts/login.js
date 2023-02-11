65eab3731d4ac63ed4c4d2abb6a15d6b4c2b2111
19Edwin92 <b_a_s_e@naver.com>
2023-02-11 17:50:37 +0900

    index_login_signup02

---
+++ static/scripts/login.js
@@ -0,0 +1,6 @@
+// let login = document.querySelector("#login")
+function login_singup() {
+  let top = (screen.height-300)/2
+      let left = (screen.width-300)/2
+      window.open('signup.html', 'open', 'width=500, height=300, top ='+top+', left='+left)
+}
